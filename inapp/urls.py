4e4564e9c5e623582c9fe05a3bf29ed8fa09267a
krushiraj <krushiraj123@gmail.com>
2018-03-31 22:17:42 +0530

    API included
    done all things just add css to make it stylish. Only part left is making conditional queries.

--- inapp/urls.py
+++ inapp/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.post_list, name='post_list'),
+    url('post_list', views.post_list, name='post_list'),
+    url('', views.latest_news, name = 'latest-news')
 ]
--- inapp/views.py
+++ inapp/views.py
@@ -1,8 +1,26 @@
 from django.shortcuts import render
 from django.utils import timezone
 from .models import Post
+from newsapi import NewsApiClient
+import json
+
+
+NEWS_API_TOKEN = '3384038df26c4de09255dd8e5dbaeb26'
 
 # Create your views here.
 def post_list(request):
         posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
         return render(request, 'inapp/post_list.html', {'posts': posts})
+
+def latest_news(request):
+        newsapi = NewsApiClient(api_key=NEWS_API_TOKEN)
+        all_articles = newsapi.get_everything(sources='bbc-news,the-verge',
+        from_parameter='2018-03-29',to='2018-03-30',language='en',sort_by='relevancy',page=2)
+        news_status = all_articles['status']
+        news_count = all_articles['totalResults']
+        news_list = all_articles['articles']
+        for i in range(len(news_list)):
+                news_list[i]['publishedAt'] = news_list[i]['publishedAt'][:10]+' at '+news_list[i]['publishedAt'][12:-1]
+        return render(request, 'inapp/latest_news.html', {'news_list':news_list, 'news_count':news_count, 'news_status':news_status})
+
+
