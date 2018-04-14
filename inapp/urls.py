8b6c64d524fc81d27b1e1d917dc7ce3de62d8b3d
samssk <sai.sameer.reddy@gmail.com>
2018-04-14 23:30:08 +0530

    changed urls

--- inapp/urls.py
+++ inapp/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-<<<<<<< HEAD
     path('', views.latest_news, name = 'latest_news'),
     path('sports/', views.sports_news, name = 'sports_news'),
     path('business/', views.business_news, name = 'business_news'),
@@ -12,8 +11,6 @@ urlpatterns = [
     path('general/', views.general_news, name = 'general_news'),
     path('science/', views.science_news, name = 'science_news'),
     path('search/', views.search_news, name = 'search_news'),
-=======
     url('post_list', views.post_list, name='post_list'),
     url('', views.latest_news, name = 'latest-news')
->>>>>>> 4d957bbabed388f0bdd4e3b1d6493fc75d02687e
 ]
